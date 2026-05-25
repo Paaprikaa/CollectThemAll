e8b95e2b472dce0b83d9f3207f485a9d8052046c
Melina Morales <melina.morales@mi.unc.edu.ar>
2026-05-25 18:01:35 -0300

    multiplayer lobby

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,25 +1,40 @@
-using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
-using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    [Header("Paneles")]
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _connecting;
     [SerializeField] private GameObject _hostPanel;
     [SerializeField] private GameObject _clientPanel;
     [SerializeField] private GameObject _panel;
+    [SerializeField] private GameObject _goBackButton;
+    [SerializeField] private TMP_InputField _ipInputField;
+    [SerializeField] private TextMeshProUGUI _yourIpText;
+    [SerializeField] private TextMeshProUGUI _wrongIpText;
+    [SerializeField] private TextMeshProUGUI _connectionErrorText;
+    [SerializeField] private TextMeshProUGUI _attempconetText;
+
+    public int maxPlayers = 2;
+
     private UnityTransport transport;
-    private string _IP;
+    private string _yourIp;
     private List<bool> _buttonSelected = new List<bool> { false, false }; // 0: host, 1: client
 
     private void Start()
     {
         transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
 
-        _IP = GetIP();
-        Debug.Log($"IP: {_IP}");
+        _yourIp = GetIP();
+
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
     }
+
+    // BUTTONS
 
     public void Host()
     {
@@ -34,6 +49,8 @@ public class MainMenuManager : MonoBehaviour
             _panel.SetActive(true);
             _buttonSelected[0] = true;
             _buttonSelected[1] = false;
+
+            _yourIpText.text = $"Your IP is: {_yourIp}\r\nShare it with your friends to join in!";
         }
         _hostPanel.SetActive(_buttonSelected[0]);
         _clientPanel.SetActive(_buttonSelected[1]);
@@ -62,6 +79,60 @@ public class MainMenuManager : MonoBehaviour
         Application.Quit();
     }
 
+    public void CreateRoom()
+    {
+        transport.SetConnectionData("0.0.0.0", 7777);
+        NetworkManager.Singleton.ConnectionApprovalCallback = ApproveConnection;
+
+        if (NetworkManager.Singleton.StartHost())
+        {
+            NetworkManager.Singleton.SceneManager.LoadScene("InGame", LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.LogError("Failed to start host.");
+        }
+    }
+
+    public void JoinRoom()
+    {
+        string ip = _ipInputField.text.Trim();
+
+        if (string.IsNullOrEmpty(ip))
+        {
+            _wrongIpText.text = "You need to enter an IP address.";
+            return;
+        }
+        if (!System.Net.IPAddress.TryParse(ip, out _))
+        {
+            _wrongIpText.text = "Invalid IP format.";
+            return;
+        }
+
+        transport.SetConnectionData(ip, 7777);
+
+
+        NetworkManager.Singleton.StartClient();
+        _mainMenu.SetActive(false);
+        _connecting.SetActive(true);
+        _attempconetText.text = $"Attempting to connect to: {ip}";
+    }
+
+    public void GoBack()
+    {
+        _mainMenu.SetActive(true);
+        _hostPanel.SetActive(false);
+        _clientPanel.SetActive(false);
+        _panel.SetActive(false);
+        _connecting.SetActive(false);
+        _goBackButton.SetActive(false);
+        _ipInputField.text = "";
+        _wrongIpText.text = "";
+        _connectionErrorText.text = "Please wait :)";
+        _attempconetText.text = "";
+    }
+
+    // NETWORKING
 
     private string GetIP()
     {
@@ -71,9 +142,42 @@ public class MainMenuManager : MonoBehaviour
             if (dir.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
             {
                 ip = dir.ToString();
-                break;
+                return ip;
             }
         }
-        return ip;
+        return "unavailable";
     }
+
+    private void OnClientDisconnected(ulong clientId)
+    {
+        if (!NetworkManager.Singleton.IsHost && clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            if (NetworkManager.Singleton.DisconnectReason != "")
+            {
+                _connectionErrorText.text = $"An error has occurred: {NetworkManager.Singleton.DisconnectReason}";
+            }
+            else
+            {
+                _connectionErrorText.text = $"An error has occurred";
+            }
+            _goBackButton.SetActive(true);
+        }
+    }
+
+    private void ApproveConnection(NetworkManager.ConnectionApprovalRequest request, NetworkManager.ConnectionApprovalResponse response)
+    {
+        int currentPlayers = NetworkManager.Singleton.ConnectedClients.Count;
+
+        if (currentPlayers < maxPlayers )
+        {
+            response.Approved = true;
+            response.CreatePlayerObject = true;
+        }
+        else
+        {
+            response.Approved = false;
+            response.Reason = $"Room is full (max {maxPlayers} players).";
+        }
+    }
+
 }
